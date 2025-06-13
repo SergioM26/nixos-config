77fea82a6c086005a0dae877785343745389c14b
SergioM26 <sergioalejandro.moreno04@gmail.com>
2025-06-13 11:46:26 -0600

    Funcional mera estructura

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    #../../modules/nixos/hyprland.nix
+
+  ];
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "samce";
@@ -51,25 +56,35 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/samce/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
+	  # Home Manager can also manage your environment variables through
+	  # 'home.sessionVariables'. These will be explicitly sourced when using a
+	  # shell provided by Home Manager. If you don't want to manage your shell
+	  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+	  # located at either
+	  #
+	  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+	  #
+	  # or
+	  #
+	  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+	  #
+	  # or
+	  #
+	  #  /etc/profiles/per-user/samce/etc/profile.d/hm-session-vars.sh
+	  #
+	  home.sessionVariables = {
+    XCURSOR_THEME = "Bibata-Modern-Ice";
+    XCURSOR_SIZE = "24";
+  # EDITOR = "emacs";
+
+};
+home.pointerCursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+      gtk.enable = true;
+    };
+
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
