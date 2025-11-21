784d889ee5f555596a0ca126037871123b4644eb
SergioM26 <sergioalejandro.moreno04@gmail.com>
2025-11-21 00:36:50 -0600

    Update

--- modules/nixos/hyprlock.nix
+++ modules/nixos/hyprlock.nix
@@ -7,91 +7,84 @@
   programs.hyprlock = {
     enable = true;
 
-    package = inputs.hyprlock.packages.${pkgs.system}.hyprlock;
+    settings = {		
 
-    settings = {
-      general = {
-        disable_loading_bar = true;
-        immediate_render = true;
-        hide_cursor = false;
-        no_fade_in = true;
-      };
+	general = {
+		hide_cursor = false;
+	};
 
-      animation = [
-        "inputFieldDots, 1, 2, linear"
-        "fadeIn, 0"
-      ];
+	animations = {
+		enabled = true;
+		bezier = linear, 1, 1, 0, 0;
+		animation = fadeIn, 1, 5, linear;
+		animation = fadeOut, 1, 5, linear;
+		animation = inputFieldDots, 1, 2, linear;
+	};
 
-      background = [
-        {
-          monitor = "";
-          path = config.theme.wallpaper;
-        }
-      ];
 
-      input-field = [
-        {
-          monitor = "eDP-1";
 
-          size = "300, 50";
-          valign = "bottom";
-          position = "0%, 10%";
+	background = {
+		monitor = ;
+		path = /home/samce/Downloads/anbuWP.png;
+		blur_passes = 3;
+	};
 
-          outline_thickness = 1;
 
-          font_color = "rgb(b6c4ff)";
-          outer_color = "rgba(180, 180, 180, 0.5)";
-          inner_color = "rgba(200, 200, 200, 0.1)";
-          check_color = "rgba(247, 193, 19, 0.5)";
-          fail_color = "rgba(255, 106, 134, 0.5)";
+	input-field = {
+		monitor =;
+		size = 30%, 10%;
+		outline_thickness = 3;
+		inner_color = "rgba(64, 64, 64, 0.0)"; # no fill
 
-          fade_on_empty = false;
-          placeholder_text = "Enter Password";
+		outer_color = "rgba(0, 0, 0, 0.0)"; # no fill 
+		check_color = "rgba(0, 0, 0, 0.0)"; # no fill
+		fail_color = "rgba(200, 0, 0, 0.0)" # no fill
 
-          dots_spacing = 0.2;
-          dots_center = true;
-          dots_fade_time = 100;
+		font_color = rgb(143, 143, 143)
+		fade_on_empty = false
+		rounding = 15
 
-          shadow_color = "rgba(0, 0, 0, 0.1)";
-          shadow_size = 7;
-          shadow_passes = 2;
-        }
-      ];
+		font_family = $font
+		placeholder_text = 
+		fail_text = $PAMFAIL
 
-      label = [
-        {
-          monitor = "";
-          text = "$TIME";
-          font_size = 150;
-          color = "rgb(b6c4ff)";
+		# uncomment to use a letter instead of a dot to indicate the typed password
+		# dots_text_format = *
+		# dots_size = 0.4
+		dots_spacing = 0.3
 
-          position = "0%, 30%";
+		# uncomment to use an input indicator that does not show the password length (similar to swaylock's input indicator)
+		# hide_input = true
 
-          valign = "center";
-          halign = "center";
+		position = 0,15%
+		halign = center
+		valign = bottom 
+	}
 
-          shadow_color = "rgba(0, 0, 0, 0.1)";
-          shadow_size = 20;
-          shadow_passes = 2;
-          shadow_boost = 0.3;
-        }
-        {
-          monitor = "";
-          text = "cmd[update:3600000] date +'%a %b %d'";
-          font_size = 20;
-          color = "rgb(b6c4ff)";
+# DATE
+label {
+  monitor =
+  text = cmd[update:1000] echo "$(date +"%A, %B %d")"
+  color = rgba(209, 207, 207, 0.75)
+  font_size = 22
+  font_family = JetBrains Mono
+  position = 0, 10%
+  halign = center
+  valign = center
+}
 
-          position = "0%, 40%";
+# TIME
+label {
+  monitor = 
+  text = cmd[update:1000] echo "$(date +"%-H:%M")"
+  color = rgba(209, 207, 207, 0.75)
+  font_size = 95
+  font_family = JetBrains Mono Extrabold
+  position = 0, 20% 
+  halign = center
+  valign = center
+}
 
-          valign = "center";
-          halign = "center";
-
-          shadow_color = "rgba(0, 0, 0, 0.1)";
-          shadow_size = 20;
-          shadow_passes = 2;
-          shadow_boost = 0.3;
-        }
-      ];
     };
   };
 }
